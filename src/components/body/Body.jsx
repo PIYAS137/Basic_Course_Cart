de2d8d8106e842d98ea504b6450f664c47835c5e
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-09-15 01:30:56 +0600

    create credit hour and remaining hour functionality

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import LeftContainer from "./LeftContainer"
 import RightContainer from "./RightContainer"
 
@@ -6,17 +6,15 @@ import RightContainer from "./RightContainer"
 const Body = () => {
 
     const [clickedData, setClickedData] = useState([])
-    const [temp, setTemp] = useState(false)
-    const [tempTitle, setTempTitle] = useState('')
-
+    const [temp, setTemp] = useState('')
+    const [totalHour,setTotalHour]=useState(0)
+    const [remainingHour,setRemainingHour]=useState(20)
 
     const doubleClickedITem = (title) => {
-        setTemp(true);
-        setTempTitle(title)
+        setTemp(title);
         setTimeout(() => {
-            setTemp(false)
-            setTempTitle('')
-        }, 2000)
+            setTemp("")
+        }, 3000)
     }
 
     const handleClickedData = (valObj) => {
@@ -26,19 +24,23 @@ const Body = () => {
                 return
             }
         }
+
+        setTotalHour(totalHour=>totalHour+valObj.credit)
+        setRemainingHour((remainingHour)=>remainingHour-valObj.credit)
+
         setClickedData([...clickedData, valObj])
     }
 
     return (
         <main>
             <div className="containerx mx-auto gap-6 grid grid-cols-4 pb-10">
-                {temp &&
-                    <div className="alert alert-error bg-red-600 text-white text-3xl p-5 absolute top-0 ">
-                        <span>You cant purchase {tempTitle} course twice !</span>
+                {temp && 
+                    <div className="alert alert-error bg-red-600 text-white text-xl p-5 absolute top-4 px-10 mx-auto rounded-xl ">
+                        <span>You cant purchase <span className="font-bold italic">{temp}</span> course twice !</span>
                     </div>
                 }
                 <LeftContainer handleClickedData={handleClickedData} />
-                <RightContainer clickedDataArr={clickedData} />
+                <RightContainer remainingHour={remainingHour} totalHours={totalHour} clickedDataArr={clickedData} />
             </div>
         </main>
     )
--- src/components/body/RightContainer.jsx
+++ src/components/body/RightContainer.jsx
@@ -1,30 +1,24 @@
 import PropTypes from 'prop-types'
-import { useState } from 'react';
 
 
 
-const RightContainer = ({ clickedDataArr }) => {
-  console.log(clickedDataArr);
-
-
+const RightContainer = ({ clickedDataArr, remainingHour, totalHours }) => {
 
 
   return (
     <div className="rounded-xl col-span-1 overflow-hidden">
       <div className="p-5 w-full shadow-xl bg-white rounded-b-xl">
-        <h1 className="text-[#2F80ED] text-lg font-bold">Credit Hour Remaining 7 hr</h1>
+        <h1 className="text-[#2F80ED] text-lg font-bold">Credit Hour Remaining {remainingHour} hr</h1>
         <hr className="block my-4" />
         <h1 className="font-bold text-xl text-black mb-5">Course Name</h1>
 
         <ul className="space-y-1">
-          <li className="  text-sm text-gray-500">1. Introduction to C Programming</li>
-          <li className="  text-sm text-gray-500">1. Introduction to C Programming</li>
-          <li className="  text-sm text-gray-500">1. Introduction to C Programming</li>
-          <li className="  text-sm text-gray-500">1. Introduction to C Programming</li>
+          {clickedDataArr.length===0 && <li className="text-sm text-gray-500">You purchase 0 course</li>}
+          {clickedDataArr.map((one,i)=><li key={i} className="text-sm text-gray-500">{i+1}. {one.title}</li>)}
         </ul>
 
         <hr className="block my-4" />
-        <h1 className="font-semibold text-gray-600">Total Credit Hour : 13</h1>
+        <h1 className="font-semibold text-gray-600">Total Credit Hour : {totalHours}</h1>
         <hr className="block my-4" />
         <h1 className="font-semibold text-gray-600">Total Price : 48000 USD</h1>
       </div>
@@ -34,6 +28,8 @@ const RightContainer = ({ clickedDataArr }) => {
 
 RightContainer.propTypes = {
   clickedDataArr: PropTypes.array.isRequired,
+  remainingHour: PropTypes.number.isRequired,
+  totalHours: PropTypes.number.isRequired
 }
 
 export default RightContainer
